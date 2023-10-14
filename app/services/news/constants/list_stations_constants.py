1c60999ca0b2549cf33f6f0f21c987b112b3644c
tarunshankarguhacogoport <Tarun.Guha@cogoport.com>
2023-10-15 00:50:38 +0530

    Add Hindi Stations

--- app/services/news/constants/list_stations_constants.py
+++ app/services/news/constants/list_stations_constants.py
@@ -8,7 +8,27 @@ HINDI_STATIONS = [
         'station_name' : 'ABP NEWS',
         'station_handle' : 'ABPNEWS',
         'station_image' : 'https://yt3.googleusercontent.com/ytc/APkrFKbZpyNco0oPLKkHbKc6qFFd3SWFEQ5kY1QCYNdPHbg=s176-c-k-c0x00ffffff-no-rj'
-    }
+    },
+    {
+        'station_name' : 'India TV',
+        'station_handle' : 'IndiaTV',
+        'station_image' : 'https://yt3.googleusercontent.com/xvSPpZd-kEa6T3KgnlWv5YYkzLxf9UjWwnjlVBhtJR67qcipOq0sBjrgOd5_46mdoCE3cglT_w=s176-c-k-c0x00ffffff-no-rj'
+    },
+    {
+        'station_name' : 'NDTV India',
+        'station_handle' : 'ndtvindia',
+        'station_image' : 'https://yt3.googleusercontent.com/ytc/APkrFKaUBpnr_EsmYR_08LnHUU1MksFgnls79N5ac0GgCQ=s176-c-k-c0x00ffffff-no-rj'
+    },
+    {
+        'station_name' : 'Republic Bharat',
+        'station_handle' : 'RepublicTVBharat',
+        'station_image' : 'https://yt3.googleusercontent.com/zv_TrbiRA-GpTz7DyYGCX08_XUcPUuVD_lcApKXQU18y3FBilr7b6CAy4Y4fVyVeiXk_JcCrg3A=s176-c-k-c0x00ffffff-no-rj'
+    },
+    {
+        'station_name' : 'Zee News',
+        'station_handle' : 'zeenews',
+        'station_image' : 'https://yt3.googleusercontent.com/vZFILuFswtjUIU5oRCc79rxRdML5A_c-D_6AdvgAAd0hqSrJihyJFkT_CTSjYwSrHgiPAAozebY=s176-c-k-c0x00ffffff-no-rj'
+    },
 ]
 
 BENGALI_STATIONS = [
