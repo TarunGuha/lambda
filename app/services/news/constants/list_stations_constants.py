d6e5a1dc89168bdb2b6ee63cc44e65541eba099c
tarunshankarguhacogoport <Tarun.Guha@cogoport.com>
2023-10-15 02:31:07 +0530

    Added Bengali Channels

--- app/services/news/constants/list_stations_constants.py
+++ app/services/news/constants/list_stations_constants.py
@@ -42,6 +42,26 @@ BENGALI_STATIONS = [
         'station_handle' : 'calcuttanews',
         'station_image' : 'https://yt3.googleusercontent.com/lE2N0oyx39a-_TZYlkaW4A1kMySqmxvu5VolBNCv4LxqE8QrzDkx8IUtZxY27WRIdhiU-LvSIMc=s176-c-k-c0x00ffffff-no-rj'
     },
+    {
+        'station_name' : 'News18 Bangla',
+        'station_handle' : 'News18Bangla',
+        'station_image' : 'https://yt3.googleusercontent.com/aC3FG7bi2xMMHLCVfrK-8E2DwdiW9xQX-qfeu7WVvqgb43CQ2349b9peG52vhWA6RquUao7emco=s176-c-k-c0x00ffffff-no-rj'
+    },
+    {
+        'station_name' : 'Republic Bangla',
+        'station_handle' : 'republicbangla',
+        'station_image' : 'https://yt3.googleusercontent.com/ytc/APkrFKaAk06mDOqPCTT7x2OW4_Yr9ALKF8b5wPhrWQ6k=s176-c-k-c0x00ffffff-no-rj'
+    },
+    {
+        'station_name' : 'TV9 Bangla',
+        'station_handle' : 'TV9BanglaLive',
+        'station_image' : 'https://yt3.googleusercontent.com/N8brWVICFyJ1QwN8aFc_ctuFvTOpJ2ng1HM4dZB3T5QE95GYQHipqW3Omi003UDlYULckDip=s176-c-k-c0x00ffffff-no-rj'
+    },
+    {
+        'station_name' : 'Zee 24 Ghanta',
+        'station_handle' : 'Zee24Ghanta',
+        'station_image' : 'https://yt3.googleusercontent.com/Uhuk7H7J-iJMiEGqtGT0mTFtzJ0Q_9W00PmN3XzkyLdeKo_2IivEs8_bAvAw77NB6pe2LR_uTw=s176-c-k-c0x00ffffff-no-rj'
+    },
 ]
 
 # {
